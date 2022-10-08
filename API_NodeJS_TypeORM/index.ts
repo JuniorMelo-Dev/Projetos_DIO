1057256ce602b6a9a4710e937d0b67f97628ccde
JuniorMelo <juniormelo.dev@gmail.com>
2022-10-07 22:37:31 -0300

    GET verb route created

--- API_NodeJS_TypeORM/index.ts
+++ API_NodeJS_TypeORM/index.ts
@@ -1,8 +1,12 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const server = express();
 
+server.get('/', (request: Request, response: Response) => {
+    return response.json({message: 'Criando método GET'})
+})
+
 server.listen(5000, () => {
-    console.log('Servidor rodando OK na porta:5000')
+    console.log('Servidor rodando OK na porta:5000 link: http://localhost:5000/')
 })
 
